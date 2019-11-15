14c41fb8e2d59a40fec7edc902412849e7b115a7
Ramona Saintandre <codinggrandma@gmail.com>
2019-11-14 19:17:54 -0500

    created cats-and-dog exercise

---
+++ code-alongs/exercises/cats-and-dogs/app.js
@@ -0,0 +1,19 @@
+// **use strict**
+
+class domesticMammals {
+    constructor(lives, babyName, behavior,annoyance){
+    this.lives = lives;
+    this.babyName = name;
+    this.behavior = behavior;
+    this.annoyance =annoyance;
+    }
+    }
+
+class Cat extends domesticMammals{
+    constructor(lives, babyName, behavior,annoyance){
+        this.lives = 9;
+        this.babyName = kitten;
+        this.behavior = cuddler;
+        this.annoyance =screach;
+}
+}
