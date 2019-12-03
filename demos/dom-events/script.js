f4ae30b406a611bb6f95610e8b193de8c95b23c1
Ramona Saintandre <codinggrandma@gmail.com>
2019-12-02 19:21:36 -0500

    created a eventlistener

--- demos/dom-events/script.js
+++ demos/dom-events/script.js
@@ -1,18 +1,19 @@
-"use strict"
-{
+"use strict" {
     const button = document.querySelector('#clickable-button');
-
+    let buttonText;
     console.log(button);
     button.addEventListener('click', event => {
         const button = event.target;
 
         const isClicked = event.dataset.clicked;
 
-        if ( !isClicked || isClicked === 'false' ) {
+        if (!isClicked || isClicked === 'false') {
+            buttonText = button.innerText;
+
             button.innerText = 'Clicked...';
             event.dataset.clicked = true;
         } else {
-            button.innerText = 'Click Me!';
+            button.innerText = buttonText;
             button.dataset.clicked = false;
         }
     });
