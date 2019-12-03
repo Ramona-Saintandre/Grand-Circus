96bbefb87c4f9b18dbdbf21b681dd269773439e5
Ramona Saintandre <codinggrandma@gmail.com>
2019-12-02 21:10:25 -0500

    added comments

--- code-alongs/wishlist/script.js
+++ code-alongs/wishlist/script.js
@@ -6,10 +6,14 @@
        e.preventDefault(); 
 
        const itemToAdd = document.querySelector('#item-to-add');
-       
+       // creating a <li>
        const itemHTML = document.createElement('li');
+
        itemHTML.classList.add('list-item');
 
+       // <li class="list-item"> Text from the input in Here </li>
+       itemHTML.innerText = itemToAdd.value;
+       
        const wishlist = document.querySelector('wishlist');
 
        wishList.appendChild(itemHTML);
