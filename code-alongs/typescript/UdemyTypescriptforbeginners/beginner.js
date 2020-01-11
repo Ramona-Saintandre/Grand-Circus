de7b3cfce4028c1761218e1d08fd0a082229d45e
Ramona Saintandre <codinggrandma@gmail.com>
2020-01-11 16:48:13 -0500

    playing around with the ts files

--- code-alongs/typescript/UdemyTypescriptforbeginners/beginner.js
+++ code-alongs/typescript/UdemyTypescriptforbeginners/beginner.js
@@ -1,4 +1,1 @@
 let hello = 'hello everyone, I am Ramona';
-const person = {
-    first_name = 'Ramona'
-};
--- code-alongs/typescript/UdemyTypescriptforbeginners/beginner.ts
+++ code-alongs/typescript/UdemyTypescriptforbeginners/beginner.ts
@@ -1,4 +1,1 @@
 let hello:string = 'hello everyone, I am Ramona';
-const person:string ={
-    first_name = 'Ramona'
-}
