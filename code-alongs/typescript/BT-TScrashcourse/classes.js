cc41e88b2888be54c9d0c3b12b5a80bd3b611c70
Ramona Saintandre <codinggrandma@gmail.com>
2020-01-10 23:52:59 -0500

    created a class, and set to private

--- code-alongs/typescript/BT-TScrashcourse/classes.js
+++ code-alongs/typescript/BT-TScrashcourse/classes.js
@@ -1,5 +1,13 @@
 var User = /** @class */ (function () {
-    function User() {
+    function User(name, email, age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        console.log('User created: ' + this.name);
     }
     return User;
 }());
+{
+}
+var John = new User('John', 'jdoe@gmail.com', 49);
+console.log(John.age);
--- code-alongs/typescript/BT-TScrashcourse/classes.ts
+++ code-alongs/typescript/BT-TScrashcourse/classes.ts
@@ -1,3 +1,21 @@
 class User{
-    
+    private name:string;
+    private email:string;
+    private age: number;
+
+    constructor(name:string,email:string,age:number){
+this.name = name;
+this.email = email;
+this.age = age;
+
+console.log('User created: '+this.name);
+
+    }
 }
+
+{
+
+}
+let John = new User('John','jdoe@gmail.com', 49);
+
+console.log(John.age);
