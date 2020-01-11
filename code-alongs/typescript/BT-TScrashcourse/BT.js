21a5cf21efc86eb598524c8c6ceb3e49c5875306
Ramona Saintandre <codinggrandma@gmail.com>
2020-01-10 23:09:50 -0500

    working with declaring different types

--- code-alongs/typescript/BT-TScrashcourse/BT.js
+++ code-alongs/typescript/BT-TScrashcourse/BT.js
@@ -1,1 +1,17 @@
 // BT crash course link https://www.youtube.com/watch?v=rAy_3SIqT-E
+var myString;
+var myNum;
+var myBool;
+var myVar;
+var strArr;
+var numArr;
+myString = 'Hi, My name is Ramona' + 'I am learning' + 'typeScript'.slice(0, 4);
+myNum = 5 + 5;
+myBool = false;
+strArr = ['Hello', 'World'];
+numArr = [1.2, 2.4, 3.6,];
+console.log(myString);
+console.log(myBool);
+console.log(myNum);
+console.log(strArr);
+console.log(numArr);
--- code-alongs/typescript/BT-TScrashcourse/BT.ts
+++ code-alongs/typescript/BT-TScrashcourse/BT.ts
@@ -1,1 +1,21 @@
 // BT crash course link https://www.youtube.com/watch?v=rAy_3SIqT-E
+let myString:string;
+let myNum: number;
+let myBool :Boolean;
+let myVar : any;
+let strArr:string[];
+let numArr: number[];
+
+myString = 'Hi, My name is Ramona'+'I am learning'+'typeScript'.slice(0,4);
+myNum = 5+5;
+myBool = false;
+strArr = ['Hello', 'World']
+numArr = [1.2,2.4,3.6,]
+
+console.log(myString);
+console.log(myBool);
+console.log(myNum);
+console.log(strArr);
+console.log(numArr);
+
+
