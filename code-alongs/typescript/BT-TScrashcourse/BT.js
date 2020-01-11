49a0d41969c8ae2cf5307ea47fc4c5dfb5f16b64
Ramona Saintandre <codinggrandma@gmail.com>
2020-01-10 23:21:07 -0500

    linked proper file , had the ts file attached

--- code-alongs/typescript/BT-TScrashcourse/BT.js
+++ code-alongs/typescript/BT-TScrashcourse/BT.js
@@ -19,6 +19,11 @@ console.log(numArr);
 var strNumTuple;
 strNumTuple = ['Hello', 4, 6, 8, 12];
 console.log(strNumTuple);
+// not sure why I am getting this error message, it is consoling out to the console though 
+// BT.ts:24:1 - error TS2322: Type '[string, number, number, number, number]' is not assignable to type '[string, number]'.
+//   Types of property 'length' are incompatible.
+//     Type '5' is not assignable to type '2'.
+// 24 strNumTuple = ['Hello',4,6,8,12]
 // void/undefined/null
 var myVoid = undefined;
 console.log(myVoid);
--- code-alongs/typescript/BT-TScrashcourse/BT.ts
+++ code-alongs/typescript/BT-TScrashcourse/BT.ts
@@ -23,7 +23,12 @@ console.log(numArr);
 let strNumTuple:[string,number]
 strNumTuple = ['Hello',4,6,8,12]
 console.log(strNumTuple);
+// not sure why I am getting this error message, it is consoling out to the console though 
+// BT.ts:24:1 - error TS2322: Type '[string, number, number, number, number]' is not assignable to type '[string, number]'.
+//   Types of property 'length' are incompatible.
+//     Type '5' is not assignable to type '2'.
 
+// 24 strNumTuple = ['Hello',4,6,8,12]
 
 // void/undefined/null
 
